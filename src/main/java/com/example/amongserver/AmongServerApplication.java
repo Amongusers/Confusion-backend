a24c62de55519f112691613098e84c8d6b0328c3
Towich <towichf@gmail.com>
2024-05-03 16:23:13 +0300

    refactor: restart server

--- src/main/java/com/example/amongserver/AmongServerApplication.java
+++ src/main/java/com/example/amongserver/AmongServerApplication.java
@@ -1,12 +1,28 @@
 package com.example.amongserver;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AmongServerApplication {
 
+	private static ConfigurableApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(AmongServerApplication.class);
+		context = SpringApplication.run(AmongServerApplication.class);
+	}
+
+	public static void restart() {
+		ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+		Thread thread = new Thread(() -> {
+			context.close();
+			context = SpringApplication.run(AmongServerApplication.class, args.getSourceArgs());
+		});
+
+		thread.setDaemon(false);
+		thread.start();
 	}
 }
