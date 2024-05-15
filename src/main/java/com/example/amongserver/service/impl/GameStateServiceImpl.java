04596a5fb38eaedafb001f1fc5f2dc1f015457a8
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-05-15 20:31:08 +0300

    fix GameState full

--- src/main/java/com/example/amongserver/service/impl/GameStateServiceImpl.java
+++ src/main/java/com/example/amongserver/service/impl/GameStateServiceImpl.java
@@ -1,20 +1,24 @@
 package com.example.amongserver.service.impl;
 
+import com.example.amongserver.domain.entity.GameCoordinates;
 import com.example.amongserver.domain.entity.GameState;
 import com.example.amongserver.domain.entity.User;
 import com.example.amongserver.dto.GameStateDto;
 import com.example.amongserver.mapper.GameStateMapper;
+import com.example.amongserver.reposirory.GameCoordinatesRepository;
 import com.example.amongserver.reposirory.GameStateRepository;
 import com.example.amongserver.service.GameStateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GameStateServiceImpl implements GameStateService {
     private final GameStateRepository gemaStateRepository;
+    private final GameCoordinatesRepository gameCoordinatesRepository;
     @Override
     public GameState add(GameState gameState) {
         return gemaStateRepository.save(gameState);
@@ -32,27 +36,42 @@ public class GameStateServiceImpl implements GameStateService {
     @Override
     public GameStateDto getGameState() {
         GameState gameState = gemaStateRepository.getById(1L);
-        boolean isAllReady = true;
-        int saveGameState;
-        for (User user : gameState.getUserList()) {
-            if (!user.isReady()) {
-                isAllReady = false;
-                break;
-            }
-        }
+
+        List<User> userList = gameState.getUserList();
+        List<User> userListNotDead = userList.stream()
+                .filter(user -> !user.isDead())
+                .toList();
+
+        boolean isAllReady = userList.stream().allMatch(User::isReady);
+
+        int saveGameState = -1;
         if (isAllReady) {
-            int totalVotes = gameState.getUserList()
-                    .stream().mapToInt(User::getNumberVotes).sum();
-            if (totalVotes==0) {
-                saveGameState = 1;
+            int totalVotes = userList.stream().mapToInt(User::getNumberVotes).sum();
+            if (totalVotes == 0) {
+                List<GameCoordinates> gameCoordinatesList = gameCoordinatesRepository
+                        .findAll().stream()
+                        .filter(gameCoordinates -> !gameCoordinates.isCompleted())
+                        .toList();
+                long imposterCount = userListNotDead.stream().filter(User::getIsImposter).count();
+                long notImposterCount = userListNotDead.size() - imposterCount;
+
+                if (imposterCount > 0 && notImposterCount > 0) {
+                    saveGameState = 2;
+                } else if ((imposterCount == 0 && notImposterCount > 0) || gameCoordinatesList.isEmpty()) {
+                    saveGameState = 3;
+                } else if ((imposterCount > 0 && notImposterCount == 0)) {
+                    saveGameState = 4;
+                }
             } else {
                 saveGameState = 2;
             }
         } else {
             saveGameState = 0;
         }
-        gameState.setGameState(saveGameState);
+        if (saveGameState == -1)
+            throw new RuntimeException();
 
+        gameState.setGameState(saveGameState);
         return GameStateMapper.toGameStateGto(gemaStateRepository.save(gameState));
     }
 }
