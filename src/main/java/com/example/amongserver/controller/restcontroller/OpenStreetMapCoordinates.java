29f6399c9fab8fee46b665702c11a5e54523ea95
n.novichkov <n.novichkov@vkteam.ru>
2024-08-17 18:48:25 +0300

    Added request with Space Ship and radius

--- src/main/java/com/example/amongserver/controller/restcontroller/OpenStreetMapCoordinates.java
+++ src/main/java/com/example/amongserver/controller/restcontroller/OpenStreetMapCoordinates.java
@@ -2,16 +2,17 @@ package com.example.amongserver.controller.restcontroller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.*;
 
 import com.example.amongserver.dto.GameCoordinatesDto;
+import com.example.amongserver.dto.OSMRequest;
+
 import de.westnordost.osmapi.OsmConnection;
 import de.westnordost.osmapi.map.data.BoundingBox;
 import de.westnordost.osmapi.map.data.LatLon;
@@ -31,15 +32,25 @@ public class OpenStreetMapCoordinates {
 
     // a piece of park "50 years of October"
     private static String BBOX = "[bbox:55.684815,37.498758,55.686050,37.501324];";
+    private static String WAY_FOOTWAY = "way[highway=footway];";
+    private static String WAY_DESIGNATED = "way[foot=designated];";
+    private static String WAY_PATH = "way[highway=path];";
 
-    @GetMapping()
-    public List<GameCoordinatesDto> getMarksInBox(){
+    @PostMapping()
+    public List<GameCoordinatesDto> getMarksInBBox(@RequestBody OSMRequest osmRequest){
+        
+        // Setup bbox
+        String bbox = GetBboxString(osmRequest);
+
         // Get nodes from OSM
-        OsmConnection connection = new OsmConnection("https://maps.mail.ru/osm/tools/overpass/api/", "my user agent"); // 
+        OsmConnection connection = new OsmConnection("https://maps.mail.ru/osm/tools/overpass/api/", "my user agent");
 	    OverpassMapDataApi overpass = new OverpassMapDataApi(connection);
 
+        String queryString = bbox + "((" + WAY_FOOTWAY + WAY_DESIGNATED + WAY_PATH + ");>;);out body;";
+        System.out.println("queryString = " + queryString);
+
         final List<Node> listOfNodes = new ArrayList<Node>();
-        overpass.queryElements(BBOX + "((way[highway=footway];way[foot=designated];);>;);out body;", new MapDataHandler()
+        overpass.queryElements(queryString, new MapDataHandler()
 		{
 			@Override 
             public void handle(BoundingBox bounds) {
@@ -64,20 +75,84 @@ public class OpenStreetMapCoordinates {
         // Generate random tasks list
         List<GameCoordinatesDto> tasksList = new ArrayList<>();
         int maxTasks = 10;
+        double MIN_DISTANCE_BETWEEN_TASKS_METRES = 50.0;
 
-        while (tasksList.size() < maxTasks) {
+        while (listOfNodes.size() > 0 && tasksList.size() < maxTasks) {
             int randomIndex = new Random().nextInt(listOfNodes.size());
             System.out.println("randomIndex = " + randomIndex);
 
             LatLon nodePos = listOfNodes.get(randomIndex).getPosition();
+
+            double nodeDistanceFromSpaceShip = calculateDistanceMetres(
+                osmRequest.getLatitudeSpaceShip(),
+                osmRequest.getLongitudeSpaceShip(), 
+                nodePos.getLatitude(),
+                nodePos.getLongitude());
+
+            if(nodeDistanceFromSpaceShip > osmRequest.getRadius()
+            ) {
+                listOfNodes.remove(randomIndex);
+                continue;
+            }
+            
+
+            boolean isSmallDistance = false;
+            for (GameCoordinatesDto task : tasksList) {
+                if(calculateDistanceMetres(
+                    task.getLatitude(), 
+                    task.getLongitude(), 
+                    nodePos.getLatitude(), 
+                    nodePos.getLongitude()) < MIN_DISTANCE_BETWEEN_TASKS_METRES
+                ) {
+                    listOfNodes.remove(randomIndex);
+                    isSmallDistance = true;
+                    break;
+                }
+            }
+            if(isSmallDistance){
+                continue;
+            }
+
+            System.out.println("norm distance = " + nodeDistanceFromSpaceShip + " m");
+
+
             tasksList.add(new GameCoordinatesDto(
                 nodePos.getLatitude(), 
                 nodePos.getLongitude(), 
-                false));
-
-            listOfNodes.remove(randomIndex);
+                false)
+            );
         }
 
         return tasksList;
+    }
+
+    private String GetBboxString(OSMRequest osmRequest){
+        double ONE_DEGREE_M = 111320;
+
+        double deltaLatitude = osmRequest.getRadius() / ONE_DEGREE_M;
+        double deltaLongitude = osmRequest.getRadius() / (ONE_DEGREE_M * Math.cos(Math.PI * osmRequest.getLatitudeSpaceShip() / 180));
+
+        double south = osmRequest.getLatitudeSpaceShip() - deltaLatitude;
+        double west = osmRequest.getLongitudeSpaceShip() - deltaLongitude;
+        double north = osmRequest.getLatitudeSpaceShip() + deltaLatitude;
+        double east = osmRequest.getLongitudeSpaceShip() + deltaLongitude;
+
+        return "[bbox:" + String.format(Locale.US, "%.6f", south) + ","
+         + String.format(Locale.US, "%.6f", west) + ","
+          + String.format(Locale.US, "%.6f", north) + ","
+           + String.format(Locale.US, "%.6f", east) + "];";
+    }
+
+    double calculateDistanceMetres(double lat1, double lon1, double lat2, double lon2) {
+        double lat1Rad = Math.toRadians(lat1);
+        double lat2Rad = Math.toRadians(lat2);
+        double lon1Rad = Math.toRadians(lon1);
+        double lon2Rad = Math.toRadians(lon2);
+    
+        double x = (lon2Rad - lon1Rad) * Math.cos((lat1Rad + lat2Rad) / 2);
+        double y = (lat2Rad - lat1Rad);
+        double distance = Math.sqrt(x * x + y * y) * 6371;
+    
+        return distance*1000;
     }
 }
---
+++ src/main/java/com/example/amongserver/dto/OSMRequest.java
@@ -0,0 +1,16 @@
+package com.example.amongserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OSMRequest {
+    private double latitudeSpaceShip;
+    private double longitudeSpaceShip;
+    private int radius;
+}
