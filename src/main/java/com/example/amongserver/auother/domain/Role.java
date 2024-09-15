f4633959c2b7e5c39cbc77f7f53fdaa75a57d72b
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-09-15 19:53:08 +0300

    add five table
    # Conflicts:
    #	src/main/java/com/example/amongserver/auother/domain/Role.java
    #	src/main/java/com/example/amongserver/auother/domain/Room.java
    #	src/main/java/com/example/amongserver/auother/domain/Task.java
    #	src/main/java/com/example/amongserver/auother/domain/TaskInRoom.java
    #	src/main/java/com/example/amongserver/auother/domain/UserInGame.java

---
+++ src/main/java/com/example/amongserver/auother/domain/Role.java
@@ -0,0 +1,27 @@
+package com.example.amongserver.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column (name = "room_class")
+    private int classs;
+
+    @Column (name = "room_type")
+    private int type;
+}
