bf7c5c96aad13b317052ce2e12005ade824ec391
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-09-22 21:05:28 +0300

    refactor temp table

--- src/main/java/com/example/amongserver/domain/entity/Vote.java
+++ src/main/java/com/example/amongserver/domain/entity/Vote.java
@@ -17,16 +17,19 @@ public class Vote {
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_uig_id", nullable = false, foreignKey = @ForeignKey(name = "vote_uig_id_fk"))
+    @JoinColumn(name = "vote_uig_id", nullable = false,
+            foreignKey = @ForeignKey(name = "vote_uig_id_fk"))
     private UserInGame userInGame;  // Ссылка на игрока в игре
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_vt_id", nullable = false, foreignKey = @ForeignKey(name = "vote_vt_id_fk"))
+    @JoinColumn(name = "vote_vt_id", nullable = false,
+            foreignKey = @ForeignKey(name = "vote_vt_id_fk"))
     private Voting voting;  // Ссылка на голосование
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_against_uig_id", nullable = false, foreignKey = @ForeignKey(name = "vote_against_uig_id_fk"))
+    @JoinColumn(name = "vote_against_uig_id", nullable = false,
+            foreignKey = @ForeignKey(name = "vote_against_uig_id_fk"))
     private UserInGame votedAgainst;  // Ссылка на игрока, против которого голосовали
 }
