9662dd667f2595a4d77d8a3ea2e24472dbd55d2a
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-09-22 14:51:43 +0300

    add TODO

--- src/main/java/com/example/amongserver/domain/entity/BaseEntityTemp.java
+++ src/main/java/com/example/amongserver/domain/entity/BaseEntityTemp.java
@@ -5,6 +5,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+
+// TODO: возможно стояит переопредить методы object
 @MappedSuperclass
 public class BaseEntityTemp {
 
