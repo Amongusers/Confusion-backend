a9fd7d724c42534288d7c49e214a47789de42046
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-10-06 20:59:36 +0300

    edit connection User @OneToOne

--- src/main/java/com/example/amongserver/registration/domain/User.java
+++ src/main/java/com/example/amongserver/registration/domain/User.java
@@ -54,8 +54,8 @@ public class User extends BaseEntityWithAudit implements UserDetails {
     )
     private Set<Authority> authorities;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Set<UserInGame> userInGameSet;
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private UserInGame userInGame;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<UserStatistic> userStatisticSet;
