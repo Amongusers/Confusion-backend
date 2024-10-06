0d62ec6b4d3888dac6b34cfeabbf7f91401d8c70
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-10-06 18:41:50 +0300

    add tree Roles

--- src/main/java/com/example/amongserver/auother/domain/Vote.java
+++ src/main/java/com/example/amongserver/auother/domain/Vote.java
@@ -26,25 +26,15 @@ public class Vote {
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_uig_id", insertable = false, updatable = false)
-//    @JoinColumn(name = "vote_uig_id", nullable = false, referencedColumnName = "id",
-//            foreignKey = @ForeignKey(name = "vote_uig_id_fk"))
+    @JoinColumn(name = "vote_uig_id", insertable = false, updatable = false,
+            foreignKey = @ForeignKey(name = "vote_uig_id_fk"))
     private UserInGame userInGame;  // Ссылка на игрока в игре
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_vt_id", insertable = false, updatable = false)
-//    @JoinColumn(name = "vote_vt_id", nullable = false, referencedColumnName = "id",
-//            foreignKey = @ForeignKey(name = "vote_vt_id_fk"))
+    @JoinColumn(name = "vote_vt_id", insertable = false, updatable = false,
+            foreignKey = @ForeignKey(name = "vote_vt_id_fk"))
     private Voting voting;  // Ссылка на голосование
-
-//    @Id
-//    @Column(name = "vote_uig_id", nullable = false)
-//    private Long userInGameId;
-//
-//    @Id
-//    @Column(name = "vote_vt_id", nullable = false)
-//    private Long votingId;
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
