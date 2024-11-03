c502ec348af280e0edcb7b001ec06edaad7d38b4
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-11-03 20:32:28 +0300

    first commit

---
+++ src/main/java/com/example/amongserver/room/JoinRoomRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.amongserver.room;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinRoomRequestDto {
+
+    private String secretCode;
+
+}
